61d53492a21f36e0b52008763718d277aaa11e07
Michael Wachter <49635794+mw8er@users.noreply.github.com>
2022-03-16 10:36:41 +0100

    skip profile

--- terraform/configuration/provider.tf
+++ terraform/configuration/provider.tf
@@ -1,12 +1,12 @@
 provider "aws" {
   region  = "us-east-1"
-  profile = var.profilename
+  # profile = var.profilename
 }
 
 provider "aws" {
   alias   = "europe"
   region  = "eu-central-1"
-  profile = var.profilename
+  # profile = var.profilename
 }
 
 # Configure the Microsoft Azure Provider
--- terraform/creation/provider.tf
+++ terraform/creation/provider.tf
@@ -1,12 +1,12 @@
 provider "aws" {
   region  = "us-east-1"
-  profile = var.profilename
+  # profile = var.profilename
 }
 
 provider "aws" {
   alias   = "europe"
   region  = "eu-central-1"
-  profile = var.profilename
+  # profile = var.profilename
 }
 
 provider "azuread" {
