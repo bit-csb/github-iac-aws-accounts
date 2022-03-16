e829796400dd3befcd5b45f8f9ef3ab98941f9aa
Michael Wachter <49635794+mw8er@users.noreply.github.com>
2022-03-16 09:34:23 +0100

    question mark already contained in sas

--- terraform/configuration/locals.tf
+++ terraform/configuration/locals.tf
@@ -4,5 +4,5 @@ locals {
 
 locals {
   new_aws_enterpries_app_script_uri = "https://${var.sa_name}.blob.core.windows.net/deploymentscripts/New-AWSEnterpriseApp.ps1?${local.sas}"
-  synchonzication_job_script_uri    = "https://${var.sa_name}.blob.core.windows.net/deploymentscripts/Enable-Provisioning.ps1?${local.sas}"
+  synchonzication_job_script_uri    = "https://${var.sa_name}.blob.core.windows.net/deploymentscripts/Enable-Provisioning.ps1${local.sas}"
 }
